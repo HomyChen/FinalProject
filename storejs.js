dbc0e0a83108d80c1bc410c87932c51ce8415fbd
Homy Chen <Homy Chen>
2016-12-11 21:14:16 -0800

    fixed order submit button

--- storejs.js
+++ storejs.js
@@ -77,16 +77,14 @@ $(document).ready(function(){
     var alpha_check = /^[a-zA-Z' ]{1,100}$/;
     var alphanum_check = /^[a-zA-Z0-9'. ]{1,200}$/;
     
-    var error_check = 'True';
-    var card_check = 'True';
+    var card_check = false;
     
     visa.onclick = function(){
         visa.style.backgroundColor = 'orange';
         visa.style.color = 'white';
         mastercard.style.backgroundColor = 'white';
         mastercard.style.color = 'dimgrey';
-        error_check = 'False';
-        card_check = 'False';
+        card_check = true;
     }
     
     mastercard.onclick = function(){
@@ -94,52 +92,57 @@ $(document).ready(function(){
         mastercard.style.color = 'white';
         visa.style.backgroundColor = 'white';
         visa.style.color = 'dimgrey';
-        error_check = 'False';
-        card_check = 'False';
+        card_check = true;
     }
+    
+    var store_email_check = false;
     
     store_email_input.onkeyup = function(){
         if (email_check.test(store_email_input.value)== true){
             store_email_input.style.color ="green";
             address_error.innerHTML = ""; 
-            error_check = 'False';
+            store_email_check = true;
         }else if (store_email_input.value == ''){
             address_error.innerHTML = "Email is required.";
-            error_check = 'True';
+            store_email_check = false;
         }else{
             store_email_input.style.color ="red";
             address_error.innerHTML = "Email can only be 3-20 characters long and must end in .com/.ca/.info/.org";
-            error_check = 'True';
+            store_email_check = false;
         }
     }
+    
+    var store_name_check = false;
     
     store_name_input.onkeyup = function(){
         if (alpha_check.test(store_name_input.value)==true){
             store_name_input.style.color = 'green';
             address_error.innerHTML = ""; 
-            error_check = 'False';
+            store_name_check = true;
         }else if (store_name_input.value == ''){
             address_error.innerHTML = "Full name is required.";
-            error_check = 'True';
+            store_name_check = false;
         }else{
             store_name_input.style.color ="red";
             address_error.innerHTML = "Your name may only contain letters.";
-            error_check = 'True';
+            store_name_check = false;
         }
     }
+    
+    var store_address1_check = false;
     
      store_address_input1.onkeyup = function(){
         if (alphanum_check.test(store_address_input1.value)==true){
             store_address_input1.style.color = 'green';
             address_error.innerHTML='';
-            error_check = 'False';
+            store_address1_check = true;
         }else if (store_address_input1.value == ''){
             address_error.innerHTML= "Address Line 1 is required.";
-            error_check = 'True';
+            store_address1_check = false;
         }else{
             store_address_input1.style.color = 'red';
             address_error.innerHTML = 'Address Line 1 cannot contain any special characters.';
-            error_check = 'True';
+            store_address1_check = false;
         }
     }
      
@@ -155,63 +158,71 @@ $(document).ready(function(){
         }
     }
     
+    var store_city_check = false;
+    
     store_city_input.onkeyup = function(){
         if (alpha_check.test(store_city_input.value)==true){
             store_city_input.style.color = 'green';
             address_error.innerHTML = ""; 
-            error_check = 'False';
+            store_city_check = true;
         }else if (store_city_input.value == ''){
             address_error.innerHTML = "City is required.";
-            error_check = 'True';
+            store_city_check = false;
         }else{
             store_city_input.style.color ="red";
             address_error.innerHTML = "City name may only contain letters.";
-            error_check = 'True';
+            store_city_check = false;
         }
     }
+    
+    var store_state_check = false;
     
     store_state_input.onkeyup = function(){
         if (alpha_check.test(store_state_input.value)==true){
             store_state_input.style.color = 'green';
             address_error.innerHTML = ""; 
-            error_check = 'False';
+            store_state_check = true;
         }else if (store_state_input.value == ''){
             address_error.innerHTML = "State/Province/Region is required.";
-            error_check = 'True';
+            store_state_check = false;
         }else{
             store_state_input.style.color ="red";
             address_error.innerHTML = "State/Province/Region name may only contain letters.";
-            error_check = 'True';
+            store_state_check = false;
         }
     }
+    
+    var store_zip_check = false;
     
     store_zip_input.onkeyup = function(){
         if (alphanum_check.test(store_zip_input.value)==true){
             store_zip_input.style.color = 'green';
             address_error.innerHTML='';
-            error_check = 'False';
+            store_zip_check = true;
         }else if (store_zip_input.value == ''){
             address_error.innerHTML= "ZIP/Postal Code is required.";
-            error_check = 'True';
+            store_zip_check = false;
         }else{
             store_zip_input.style.color = 'red';
             address_error.innerHTML = 'ZIP/Postal Code cannot contain any special characters.';
-            error_check = 'True';
+            store_zip_check = false;
         }
     }
+    
+    var store_country_check = false;
     
     store_country_input.onkeyup = function(){
         if (alpha_check.test(store_country_input.value)==true){
             store_country_input.style.color = 'green';
             address_error.innerHTML = ""; 
-            error_check = 'False';
+            store_country_check = true;
         }else if (store_country_input.value == ''){
             address_error.innerHTML = "Country is required.";
-            error_check = 'True';
+            store_country_check = false;
         }else{
             store_country_input.style.color ="red";
             address_error.innerHTML = "Country name may only contain letters.";
-            error_check = 'True';
+            store_country_check = false;
         }
     }
     
@@ -228,51 +239,55 @@ $(document).ready(function(){
     }
     
     var card_num_check = /^[0-9]{16}$/;
+    var card_num_input_check = false;
     
     card_num.onkeyup = function(){
         if(card_num_check.test(card_num.value)==true){
             card_num.style.color = 'green';
             payment_error.innerHTML='';
-            error_check = 'False';
+            card_num_input_check = true;
         }else if (card_num.value == ''){
             payment_error.innerHTML= "Card number is required";
-            error_check = 'True';
+            card_num_input_check = false;
         }else{
             card_num.style.color = 'red';
             payment_error.innerHTML = 'Card number must contain only numbers and be 16 digits long. Enter your number with no spaces or dashes';
-            error_check = 'True';
+            card_num_input_check = false;
         }
     }
+    
+    var card_name_check = false;
     
     name_card.onkeyup = function(){
         if (alpha_check.test(name_card.value)==true){
             name_card.style.color = 'green';
             payment_error.innerHTML = ""; 
-            error_check = 'False';
+            card_name_check = true;
         }else if (name_card.value == ''){
             payment_error.innerHTML = "Your full name, as written on the card, is required.";
-            error_check = 'True';
+            card_name_check = false;
         }else{
             name_card.style.color ="red";
             payment_error.innerHTML = "Your name may only contain letters.";
-            error_check = 'True';
+            card_name_check = false;
         }
     }
     
     var card_exp_check = /^[0-1][1-9]\/[0-9][0-9]$/;
+    var card_exp_input_check = false;
     
     card_exp.onkeyup = function(){
         if (card_exp_check.test(card_exp.value)==true){
             card_exp.style.color = 'green';
             payment_error.innerHTML = ""; 
-            error_check = 'False';
+            card_exp_input_check = true;
         }else if (card_exp.value == ''){
             payment_error.innerHTML = "Credit card expiration date is required.";
-            error_check = 'True';
+            card_exp_input_check = false;
         }else{
             card_exp.style.color ="red";
             payment_error.innerHTML = "Credit card expiration date must be written in the format mm/yy. For example, 03/15 for March 2015";
-            error_check = 'True';
+            card_exp_input_check = false;
         }
     }
     
@@ -292,15 +307,16 @@ $(document).ready(function(){
     var order_item_img = document.getElementById("order_item_img");
     
     order_next.onclick = function(){
-        if (error_check == 'True' || card_check == 'True' || store_email_input.value == '' || store_name_input.value == '' || store_address_input1.value == '' || store_city_input.value == '' || store_state_input.value == '' || store_zip_input.value == '' || store_country_input.value == '' || name_card.value == '' || card_num.value == '' || card_exp.value == ''){
-            payment_error.innerHTML = 'Please ensure the form is filled out properly.';
-        }else{
+        if (card_check == true && store_email_check == true && store_name_check == true && store_address1_check == true && store_city_check == true && store_state_check == true && store_zip_check == true && store_country_check == true && card_num_input_check == true && card_name_check == true && card_exp_input_check == true){
             final_page.style.marginLeft = '15vw';
             order_confirm.style.marginLeft = '100vw';
             order_confirm.style.transition = '0s';
             inputted_name.innerHTML = store_name_input.value;
             inputted_email.innerHTML = store_email_input.value;
-            inputted_address.innerHTML = store_address_input2.value+' '+store_address_input1.value+' '+store_city_input.value+', '+store_state_input.value+', '+store_country_input.value+' '+store_zip_input.value;}
+            inputted_address.innerHTML = store_address_input2.value+' '+store_address_input1.value+' '+store_city_input.value+', '+store_state_input.value+', '+store_country_input.value+' '+store_zip_input.value;
+        }else{
+            payment_error.innerHTML = 'Please ensure the form is filled out properly.';
+            }
     }
     
      order_exit.onclick = function(){
@@ -320,6 +336,17 @@ $(document).ready(function(){
         name_card.value = '';
         card_num.value = '';
         card_exp.value = '';
+        card_check = false;
+        store_email_check = false;
+        store_name_check = false;
+        store_address1_check = false;
+        store_city_check = false;
+        store_state_check = false;
+        store_zip_check = false;
+        store_country_check = false;
+        card_num_input_check = false;
+        card_name_check = false;
+        card_exp_input_check = false;
     }
     
     store_gold.onclick = function(){
